ddce82a1f3002e87944a52c00dbfbc9b133c1ad4
marinred <endrms1995@gmail.com>
2023-01-05 21:38:37 +0900

    Refactor: Review black pep 적용 #22
    - review black pep 적용
    
    Close #22

--- review/views.py
+++ review/views.py
@@ -3,14 +3,25 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from review.models import Review
-from review.serializers import ReviewSerializer, ReviewCreateSerializer
+from musics.models import Music
+from review.serializers import (
+    ReviewSerializer,
+    ReviewCreateSerializer,
+    ReviewMusicSerializer,
+)
+
 
 class ReviewView(APIView):
-    
+    def get(self, request, music_id):
+        music = Music.objects.get(id=music_id)
+        comment = music.review_set.all()
+        serializer = ReviewMusicSerializer(comment, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request, music_id):
         serializer = ReviewCreateSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(user=request.user, music_id=music_id,rank=5)
+            serializer.save(user=request.user, music_id=music_id, rank=5)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -20,18 +31,17 @@ class ReviewView(APIView):
         if request.user == review.user:
             serializer = ReviewCreateSerializer(review, data=request.data)
             if serializer.is_valid():
-                serializer.save() 
+                serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response("권한이 없습니다!" , status=status.HTTP_403_FORBIDDEN)
-
+            return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
 
     def delete(self, request, music_id, review_id):
         review = get_object_or_404(Review, id=review_id)
         if request.user == review.user:
             review.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response("삭제가 완료되었습니다.", status=status.HTTP_204_NO_CONTENT)
         else:
-            return Response("권한이 없습니다!" , status=status.HTTP_403_FORBIDDEN)
+            return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
